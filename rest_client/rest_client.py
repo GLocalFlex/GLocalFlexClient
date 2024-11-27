551e100500be406ad1a4c28609b006257798c3bb
Stephan Mehnert <stephan.mehnert@vtt.fi>
2024-11-27 14:06:42 +0000

    refactoring

--- rest_client/rest_client.py
+++ rest_client/rest_client.py
@@ -1,4 +1,5 @@
 
+from curses.ascii import SI
 import time
 import random
 import argparse
@@ -11,77 +12,126 @@ import urllib3
 import requests
 from auth import Auth
 
+from utils import SellerBuyerSettings, cli_args, DefaultConfig, HostConfig, UserConfig, EndpointConfig, Parameters
+from utils import Side
 import warnings
 warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 
 
-TZ = dt.timezone.utc
 HOST = os.getenv("GFLEX_URL", "test.glocalflexmarket.com")
 CLIENT_ID = "glocalflexmarket_public_api"
 AUTH_ENDPOINT = "/auth/oauth/v2/token"
 ORDER_ENDPOINT = "/api/v1/order/"
 SSL_VERIFY = True
+DEFAULT_SLEEP_TIME = 1 # sleep time between cycles
+DEFAULT_RUN_TIME = 0 # run time in seconds, 0 runs forever
+# COUNTRY_CODES = ["CZ", "DE", "CH", "ES", "FI", "FR", ""]
+COUNTRY_CODES_ALLOWED = ["CZ"]
+
+HTTP_AUTHENTICATION_ERROR = 401
+
+baseline_data = {
+            "metering_id": "ce15bc33-2bda-4d26-8ddd-b958b22b569b",
+            "created_at": "2019-08-24T14:15:00Z",
+            "reading_start": "2019-08-24T14:15:00Z",
+            "reading_end": "2019-08-24T14:30:00Z",
+            "resolution": 1,
+            "direction": "consumption",
+            "quality": "estimated",
+            "energy_product": "apparent",
+            "unit_measured": "kWh",
+            "data_points": [{"2019-08-24T14:15:00Z": 1}]
+    }
 
 
-# default values
 
-SLEEP_TIME = 1 # sleep time between cycles
-RUN_TIME = 0 # run time in seconds, 0 runs forever
-
-@dataclass
-class SellerBuyerSettings:
-    """
-    Dataclass for storing the settings for the buyer and seller.
-
-    quantity will be in the range btween min and max
-    price will be in the range btween min and max
-    wait multiplier is multiplied with sleep time 
-    """
-    quantity_min: int 
-    quantity_max: int
-    unit_price_min: float
-    unit_price_max: float
-    wait_multiplier_min: int
-    wait_multiplier_max: int
-
-# Buyer and seller settings
-
-BUYER = SellerBuyerSettings(quantity_min=1000,
+config = DefaultConfig(
+    market=HostConfig(
+        host=HOST,
+        client_id=CLIENT_ID,
+        ssl_verify=SSL_VERIFY,
+        api=EndpointConfig(
+            auth=AUTH_ENDPOINT,
+            order=ORDER_ENDPOINT
+        ),
+    ),
+    user=UserConfig(
+        username=None,
+        password=None,
+    ),
+    params=Parameters(
+        runtime=DEFAULT_RUN_TIME,
+        frequency=DEFAULT_SLEEP_TIME,
+        timezone=dt.timezone.utc,
+        side=Side.BUY,
+    ),
+    buyer=SellerBuyerSettings(
+                quantity_min=1000,
                 quantity_max=2000,
                 unit_price_min=0.5,
                 unit_price_max=1.5,
                 wait_multiplier_min=1,
-                wait_multiplier_max=5)
-
-SELLER = SellerBuyerSettings(quantity_min=100,
+                wait_multiplier_max=5,
+                ),
+    seller=SellerBuyerSettings(
+                quantity_min=100,
                 quantity_max=10000,
                 unit_price_min=0.1,
                 unit_price_max=1,
                 wait_multiplier_min=1,
-                wait_multiplier_max=5)
+                wait_multiplier_max=5,
+                baseline=baseline_data,
+                )
+)
 
 @dataclass
-class OrderParameters:
+class Order:
     side: str
     location_ids: list[str]
     country_code: str
     quantity: float
     price: float
+    delivery_start: dt.datetime = None
+    delivery_end: dt.datetime = None
+    expiry_time: dt.datetime = None
+    baseline: dict = None
 
     def as_dict(self) -> dict:
-        return self.__dict__
+        d = self.__dict__
+        if self.side == Side.BUY:
+            # remove baseline key from buy order
+            d.pop("baseline")
+        
+        return {
+            "side": self.side,
+            "power": self.quantity,
+            "price": self.price,
+            "delivery_start": self.delivery_start,
+            "delivery_end": self.delivery_end,
+            "expiry_time": self.expiry_time,
+            "location": {"location_id": self.location_ids,
+                        "country_code": self.country_code}
+        }
+
 
 class Client:
     """GLocalFlex REST client"""
 
-    def __init__(self, username: str, password: str, client_id: str, host: str, auth_endpoint: str, order_endpoint : str, timezone, verify=True) -> None:
+    def __init__(self, config: DefaultConfig) -> None:
 
+        self.config = config
         #Define order_url here, all other necessary stuff is defined by Authenticate.
-        self.order_url = f"https://{host}{order_endpoint}" 
-        self.verify = verify
-        self.auth: Auth = Auth(username, password, client_id, host, auth_endpoint, timezone, verify) 
+        self.order_url = f"https://{config.market.host}{config.market.api.order}" 
+        self.verify = config.market.ssl_verify
+        self.auth: Auth = Auth(config.user.username,
+                               config.user.password,
+                               config.market.client_id,
+                               config.market.host,
+                               config.market.api.auth,
+                               config.params.timezone,
+                               config.market.ssl_verify) 
 
-    def create_order(self, order: dict) -> requests.Response:
+    def send_order(self, order: dict) -> requests.Response:
         """Send post request to marketplace."""
 
         headers = {
@@ -92,155 +142,123 @@ class Client:
         return response
 
 
-def set_values(values: SellerBuyerSettings, side: str) -> OrderParameters:
+    def set_order_parameters(self, values: SellerBuyerSettings, side: str, baseline: dict) -> Order:
+            def format_time(time):
+                return time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
 
-        wmin = values.wait_multiplier_min
-        wmax = values.wait_multiplier_max
+            def round_quarter(t: dt.datetime) -> dt.datetime:
+                """Round the time to the nearest quarter hour 00, 15, 30, 45"""
+                minute = t.minute
+                if minute % 15 != 0:
+                    t += dt.timedelta(minutes=15 - (minute % 15))
+                return t.replace(second=0, microsecond=0)
+            
+            wmin = values.wait_multiplier_min
+            wmax = values.wait_multiplier_max
+            quantity = random.randrange(values.quantity_min, values.quantity_max, 100)
+            price = round(random.uniform(values.unit_price_min, values.unit_price_max), 2)    
+            country_code = random.choice(COUNTRY_CODES_ALLOWED)
 
-        quantity = random.randrange(values.quantity_min, values.quantity_max, 100)
-        price = round(random.uniform(values.unit_price_min, values.unit_price_max), 2)    
-        country_code = random.choice(["CZ", "DE", "CH", "ES", "FI", "FR", ""]) # optional CZ, DE, CH, FI, ES
-        # country_code = random.choice(["FI"]) # optional CZ, DE, CH, FI, ES
-        # location_ids = random.choice([[None], ["loc1", "loc2", "loc3"], ["loc.*"]])
-        location_ids = [None]
+            location_ids = [None]
 
-        return OrderParameters(side=side,
-                            location_ids=location_ids,
-                            country_code=country_code,
-                            quantity=quantity,
-                            price=price), wmin, wmax
+            time_now = dt.datetime.now(self.config.params.timezone)
+
+            return Order(side=side,
+                        location_ids=location_ids,
+                        country_code=country_code,
+                        quantity=quantity,
+                        price=price,
+                        delivery_start= format_time(round_quarter(time_now + dt.timedelta(hours=1))),
+                        delivery_end=format_time(round_quarter(time_now + dt.timedelta(hours=2))),
+                        expiry_time=format_time(time_now + dt.timedelta(minutes=10)),
+
+                        baseline=baseline
+                        ), wmin, wmax
+        
+    @staticmethod
+    def log_response(code: int, text: str, params: Order) -> None:
+        if code == 200:
+            logging.info(f'status={code}, side={params.side}, power={params.quantity}, '
+                        f'price={params.price}, country={params.country_code}, loc_ids={params.location_ids}')  
+        elif code == 401:
+            logging.debug(f'Debug: 401 Get new token.')
+        elif code == 429:
+            logging.warning(f'Warning: 429 Too many requests.')
+        elif code == 422:
+            logging.error(f'Error: 422 Unprocessable Content: {text}.')
+        else:
+            logging.error(f'Request failed with code {code}')
+
         
 
-# def format_order(self, side: str, quantity: int, unit_price: float, loc_ids: list[str], country_code: str) -> dict:
-def format_order(params: OrderParameters) -> dict:
-    """Format the order data to be suitable for the server."""
-    
-    def format_time(time):
-        return time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
 
-    def round_quarter(t: dt.datetime) -> dt.datetime:
-        """Round the time to the nearest quarter hour 00, 15, 30, 45"""
-        minute = t.minute
-        if minute % 15 != 0:
-            t += dt.timedelta(minutes=15 - (minute % 15))
-        return t.replace(second=0, microsecond=0)
-
-
-    time_now = dt.datetime.now(TZ)
-
-    delivery_start = round_quarter(time_now + dt.timedelta(hours=1))
-    delivery_end = round_quarter(time_now + dt.timedelta(hours=2))
-    expiry_time = time_now + dt.timedelta(minutes=10)
-
-    return {
-        "side": params.side,
-        "power": params.quantity,
-        "price": params.price,
-        "delivery_start": format_time(delivery_start),
-        "delivery_end": format_time(delivery_end),
-        "expiry_time": format_time(expiry_time),
-        "location": {"location_id": params.location_ids,
-                    "country_code": params.country_code}
-    }
-
-def log_response(code: int, text: str, params: OrderParameters) -> None:
-    if code == 200:
-        logging.info(f'status={code}, side={params.side}, power={params.quantity}, '
-                    f'price={params.price}, country={params.country_code}, loc_ids={params.location_ids}')  
-    elif code == 401:
-        logging.debug(f'Debug: 401 Get new token.')
-    elif code == 429:
-        logging.warning(f'Warning: 429 Too many requests.')
-    elif code == 422:
-        logging.error(f'Error: 422 Unprocessable Content: {text}.')
-    else:
-        logging.error(f'Request failed with code {code}')
-
+    def run(self):
         
-def cli_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Create buy or sell orders")
-    parser.add_argument('side', choices=['buy', 'sell'])
-    parser.add_argument("-r", "--run", dest="run_time", metavar="", type=int, default=RUN_TIME, help=f"Running time in seconds. 0 runs forever. Default: {RUN_TIME}")
-    parser.add_argument("-s", "--sleep", dest="sleep_time", metavar="", type=float, default=SLEEP_TIME, help=f"Sleep time per cycle. Default: {SLEEP_TIME}")
-    parser.add_argument('--log', dest='log', action='store_true')
-    parser.add_argument("--host", default=HOST, dest="host", metavar="", help=f"Host url, DEFAULT: {HOST}")
-    parser.add_argument("-u", dest="username", metavar="", help=f"Username")
-    parser.add_argument("-p", dest="password", metavar="", help=f"Password")
-    return parser.parse_args()
 
-def run(side: str, run_time: int, sleep_time: int, args: argparse.Namespace):
-    
-    client_id = CLIENT_ID
-    auth_endpoint = AUTH_ENDPOINT
-    order_endpoint = ORDER_ENDPOINT
-    timezone = TZ
-    verify = SSL_VERIFY
+        logging.info(f"Target url {self.config.market.host}")
+        self.auth.token_new()
 
-    host = args.host
-    username = args.username
-    password = args.password
-    
+        starttime = time.time() 
+        while True:
+            
+            if self.config.params.runtime != 0: #setting to 0 runs forever
+                if time.time() > starttime + self.config.params.runtime:
+                    break
 
-    logging.info(f"Target url {host}")
-    user = Client(username, password, client_id, host, auth_endpoint, order_endpoint, timezone, verify=verify)
-    user.auth.token_new()
+            if self.auth.token_check_expiry():
+                if not self.auth.token_refresh():
+                    while not self.auth.token_new():
+                        time.sleep(5)
 
-    starttime = time.time() 
-    while True:
-        
-        if side == 'buy':
-            params, wmin, wmax = set_values(BUYER, 'buy')
-        if side == 'sell':
-            params, wmin, wmax = set_values(SELLER, 'sell')
-
-        if run_time != 0: #setting to 0 runs forever
-            if time.time() > starttime + run_time:
-                break
-
-        if user.auth.token_check_expiry():
-            if not user.auth.token_refresh():
-                while not user.auth.token_new():
-                    time.sleep(5)
-                    
-
-        """Makes the order and logs result."""
-        order = format_order(params)
-
-        try:
-            order_status = user.create_order(order)
-        except (ssl.SSLError) as e:
-            logging.error(f'Error: {e}')
-            time.sleep(5)
-            continue
-        except (urllib3.exceptions.NewConnectionError, requests.exceptions.ConnectionError) as e:
-            logging.error(f'Marketplace is not available')
-            logging.error(f'Error: {e}')
-            time.sleep(5)
-            continue
-
-        if order_status.status_code == 401:
-            while not user.auth.token_new():
+            if self.config.params.side == Side.BUY:
+                order, wmin, wmax = self.set_order_parameters(config.buyer, Side.BUY, baseline_data)
+            if self.config.params.side == Side.SELL:
+                order, wmin, wmax = self.set_order_parameters(config.seller, Side.SELL, baseline_data)
+              
+            try:
+                response = self.send_order(order.as_dict())
+            except (ssl.SSLError) as e:
+                logging.error(f'Error: {e}')
                 time.sleep(5)
-        log_response(order_status.status_code, order_status.text, params)
-        sleep_multiplier  = random.randint(wmin, wmax)
-        time.sleep(sleep_time*sleep_multiplier)
-    
-    logging.info(f'Finished, side: {params.side}')
+                continue
+            except (urllib3.exceptions.NewConnectionError, requests.exceptions.ConnectionError) as e:
+                logging.error(f'Marketplace is not available')
+                logging.error(f'Error: {e}')
+                time.sleep(5)
+                continue
+
+            if response.status_code == HTTP_AUTHENTICATION_ERROR:
+                while not self.auth.token_new():
+                    time.sleep(5)
+
+            self.log_response(response.status_code, response.text, order)
+
+            sleep_multiplier  = random.randint(wmin, wmax)
+            time.sleep(self.config.params.frequency*sleep_multiplier)
+        
+        logging.info(f'Finished, side: {order.side}')
 
 
 def main():
-    args = cli_args()
-    side = args.side
-    run_time = args.run_time
-    log = args.log
-    sleep_time = args.sleep_time
+    args = cli_args(config)
+    config.market.host = args.host
+    config.user.username = args.username
+    config.user.password = args.password
+    config.params.runtime = args.run_time
+    config.params.frequency = args.sleep_time
+    config.params.log = args.log
+    config.params.side = Side(args.side)
 
-    if log == True:
+
+    
+    client = Client(config)
+
+    if config.params.log == True:
         logging.basicConfig(encoding='utf-8', level=logging.INFO, format='%(asctime)s %(message)s')
-        logging.info(f'Client started with runtime: {run_time}, side: {side}')
+        logging.info(f'Client started with runtime: {config.params.runtime}, side: {config.params.side}')
 
     try:
-        run(side, run_time, sleep_time, args)
+        client.run()
     except KeyboardInterrupt:
         pass
 
---
+++ rest_client/utils.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+import argparse
+from enum import StrEnum
+
+class Side(StrEnum):
+    BUY = 'buy'
+    SELL = 'sell'
+
+
+@dataclass
+class SellerBuyerSettings:
+    """
+    Dataclass for storing the settings for the buyer and seller.
+
+    quantity will be in the range btween min and max
+    price will be in the range btween min and max
+    wait multiplier is multiplied with sleep time 
+    """
+    quantity_min: int 
+    quantity_max: int
+    unit_price_min: float
+    unit_price_max: float
+    wait_multiplier_min: int
+    wait_multiplier_max: int
+    baseline: dict = None
+
+
+
+
+@dataclass
+class UserConfig:
+    username: str = None
+    password: str = None
+
+@dataclass
+class EndpointConfig:
+    auth: str
+    order: str
+
+@dataclass
+class Parameters:
+    side: str
+    runtime: float
+    frequency: float
+    timezone: str
+    log: bool = False
+
+@dataclass
+class HostConfig:
+    host: str
+    client_id: str
+    ssl_verify: bool
+    api: EndpointConfig
+
+@dataclass
+class DefaultConfig:
+    market: HostConfig
+    user: UserConfig
+    params: Parameters
+    buyer: SellerBuyerSettings = None
+    seller: SellerBuyerSettings = None
+
+def cli_args(config: DefaultConfig) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create buy or sell orders")
+    parser.add_argument('side', choices=['buy', 'sell'])
+    parser.add_argument("-r", "--run", dest="run_time", metavar="", type=int, default=config.params.runtime, help=f"Running time in seconds. 0 runs forever. Default: {config.params.runtime}")
+    parser.add_argument("-s", "--sleep", dest="sleep_time", metavar="", type=float, default=config.params.frequency, help=f"Sleep time per cycle. Default: {config.params.frequency}")
+    parser.add_argument('--log', dest='log', action='store_true')
+    parser.add_argument("--host", default=config.market.host, dest="host", metavar="", help=f"Host url, DEFAULT: {config.market.host}")
+    parser.add_argument("-u", dest="username", metavar="", help=f"Username")
+    parser.add_argument("-p", dest="password", metavar="", help=f"Password")
+    return parser.parse_args()    
